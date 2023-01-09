ec14cd8578a066223c2a77336e27795fc3f69b3b
usrlocal <usrlocal@GA319-FE14.ad.univ-angers.fr>
2023-01-09 16:33:54 +0100

    tamere

---
+++ JavaProgSujet/src/myPackage/CompteCourant.java
@@ -0,0 +1,20 @@
+package myPackage;
+
+public class CompteCourant {
+	
+	private double _solde;
+	
+	public CompteCourant(double solde) {
+		_solde = solde;
+	}
+	
+	public void depotCompte(double qteC) {
+		_solde += qteC;
+	}
+	
+	public double getSolde() {
+		return _solde;
+	}
+	
+
+}
